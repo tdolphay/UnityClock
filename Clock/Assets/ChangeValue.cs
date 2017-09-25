a11c0b57dfd0707b0332493c330d318361f2bc9e
tdolphay <tad.dolphay@gmail.com>
2017-09-24 19:14:30 -0500

    Added Game

---
+++ Clock/Assets/ChangeValue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class ChangeValue : MonoBehaviour {
+	public TextMesh hour, minute;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DateTime time = DateTime.Now;
+
+		int hourValue = time.Hour;
+		int minuteValue = time.Minute;
+
+		if (hourValue >= 0 && hourValue <= 9) {
+			hour.text = "0" + hourValue.ToString();
+		} else {
+			hour.text = hourValue.ToString();
+		}
+
+		if (minuteValue >= 0 && minuteValue <= 9) {
+			minute.text = "0" + minuteValue.ToString();
+		} else {
+			minute.text = minuteValue.ToString();
+		}
+	}
+}
---
+++ Clock/Assets/Clock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+	const float degreesPerHour = 30f, degreesPerMinute = 6f, degreesPerSecond = 6f;
+	public Transform hoursTransform, minutesTransform, secondsTransform;
+	public bool continuous;
+
+	void Awake() {
+		
+
+		if (continuous) {
+			TimeSpan time = DateTime.Now.TimeOfDay;
+
+			hoursTransform.localRotation = Quaternion.Euler (0f, (float) time.TotalHours * degreesPerHour, 0f);
+			minutesTransform.localRotation = Quaternion.Euler (0f, (float) time.TotalMinutes * degreesPerMinute, 0f);
+			secondsTransform.localRotation = Quaternion.Euler (0f, (float) time.TotalSeconds * degreesPerSecond, 0f);
+		} else {
+			DateTime time = DateTime.Now;
+
+			hoursTransform.localRotation = Quaternion.Euler (0f, time.Hour * degreesPerHour, 0f); // Rotation storage. Multiply hour by 30 to get correct rotation around center
+			minutesTransform.localRotation = Quaternion.Euler (0f, time.Minute * degreesPerMinute, 0f);
+			secondsTransform.localRotation = Quaternion.Euler (0f, time.Second * degreesPerSecond, 0f);
+		}
+	}
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Awake();
+	}
+}
